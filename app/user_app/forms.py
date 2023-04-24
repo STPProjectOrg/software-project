2f7fafc1f76e672d54a217f94abb783fd2f4a781
dido3296 <dimitri.dorn2@stud.hs-flensburg.de>
2023-04-24 17:12:12 +0200

    STP-38 LoginView und LoginForm erstellt

--- app/user_app/forms.py
+++ app/user_app/forms.py
@@ -36,25 +36,6 @@ class UserForm(forms.ModelForm):
         self.set_form_control()
 
 
-        '''
-            if hasattr(self, 'cleaned_data') and field_name in self.cleaned_data and self.cleaned_data[field_name]:
-                if field_name in self.errors:
-                    field.widget.attrs['class'] = 'form-control is-invalid'
-                else:
-                    field.widget.attrs['class'] = 'form-control is-valid'
-            else:
-                field.widget.attrs['class'] = 'form-control'
-
-
-            
-            if field_name in self.errors:
-                field.widget.attrs['class'] = 'form-control is-invalid'
-            elif field_name in self.fields:
-                field.widget.attrs['class'] = 'form-control is-valid'
-            else:
-                field.widget.attrs['class'] = 'form-control'
-            '''
-
     
 
 class UserProfileInfoForm(forms.ModelForm):
