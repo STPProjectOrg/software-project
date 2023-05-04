0d9e91a32c4d25849a247d49277bd8f801cdf179
Maximilian Steinberg <maximilian.steinberg@stud.hs-flensburg.de>
2023-05-04 12:25:15 +0200

    STP-67 Add components as custom_tag

--- app/core/templatetags/core_tags.py
+++ app/core/templatetags/core_tags.py
@@ -6,3 +6,13 @@ register = template.Library()
 @register.inclusion_tag("inclusion/logo_name.html")
 def logo_name():
     return
+
+
+@register.inclusion_tag("inclusion/navigation.html")
+def navigation():
+    return
+
+
+@register.inclusion_tag("inclusion/footer.html")
+def footer():
+    return
