7c1aa7c7546fb061d3a1883a51d5938a85e01cc0
Maximilian Steinberg <maximilian.steinberg@stud.hs-flensburg.de>
2023-05-04 13:01:31 +0200

    STP-29 Add SSC responsive base_html

--- app/core/templatetags/core_tags.py
+++ app/core/templatetags/core_tags.py
@@ -2,6 +2,8 @@ from django import template
 
 register = template.Library()
 
+# inclusion_tags
+
 
 @register.inclusion_tag("inclusion/logo_name.html")
 def logo_name():
@@ -16,3 +18,8 @@ def navigation():
 @register.inclusion_tag("inclusion/footer.html")
 def footer():
     return
+
+
+@register.inclusion_tag("inclusion/search.html")
+def search(width):
+    return {'width': width}
