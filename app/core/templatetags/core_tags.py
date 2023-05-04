cb21bd54174e74c6083a3fdc7a517a883c415034
Maximilian Steinberg <maximilian.steinberg@live.de>
2023-05-04 13:31:08 +0200

    Update base_html

--- app/core/templatetags/core_tags.py
+++ app/core/templatetags/core_tags.py
@@ -2,7 +2,24 @@ from django import template
 
 register = template.Library()
 
+# inclusion_tags
+
 
 @register.inclusion_tag("inclusion/logo_name.html")
 def logo_name():
     return
+
+
+@register.inclusion_tag("inclusion/navigation.html")
+def navigation():
+    return
+
+
+@register.inclusion_tag("inclusion/footer.html")
+def footer():
+    return
+
+
+@register.inclusion_tag("inclusion/search.html")
+def search(width):
+    return {'width': width}
