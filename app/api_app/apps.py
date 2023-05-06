3e374553bdc1eb21f4edcc73828fceaa12c91e53
Nico Isheim <nico.isheim@tutanota.com>
2023-05-06 14:55:59 +0200

    Add dailyScheduler

--- app/api_app/apps.py
+++ app/api_app/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class ApiAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api_app'
+
+    # Um den scheduler zu starten nachdem das Projekt vollständig initialisiert wurde,
+    # muss die Methode ready() überschrieben werden.
+    # Leider muss der Import vom scheduler hier stehen, da es sonst zu einem Zirkulären Import kommt.
+    def ready(self):
+        from .scheduler import start_scheduler
+        start_scheduler()
---
+++ app/api_app/scheduler.py
@@ -0,0 +1,43 @@
+import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from app.settings import TIME_ZONE
+from .views import addCoinToDatabase
+
+# Wir bräuchten hier eine Liste mit allen Coins, die wir in der Datenbank haben wollen.
+allCurrencys = {
+    '0': {
+        'identifier': 'BTC',
+        'name': 'Bitcoin',
+        'currency': 'EUR'
+    }
+}
+
+
+# Der scheduler wird mit dieser Methode gestartet.
+# Die Funktion update_data() wird jeden Tag um 03:00 Uhr ausgeführt.
+def start_scheduler():
+    scheduler = BackgroundScheduler(timezone=TIME_ZONE)
+    scheduler.add_job(
+        update_data,
+        'cron',
+        day_of_week="mon-sun",
+        hour=3
+    )
+    scheduler.start()
+
+
+# Diese Funktion wird vom Scheduler aufgerufen.
+# Der Aufruf der Methode addCoinToDatabase() fügt die Coins der Datenbank hinzu.
+# Eventuell müssen die Fehler noch gefangen werden. -> Mit Julian besprechen
+def update_data():
+    print("scheduler: NEW API UPDATE INITIALIZED")
+    for i in range(len(allCurrencys)):
+        concreteCurrency = allCurrencys.get("%i" % i)
+        print(allCurrencys)
+        print(concreteCurrency)
+        addCoinToDatabase(
+            concreteCurrency.get('identifier'),
+            concreteCurrency.get('name'),
+            concreteCurrency.get('currency')
+        )
