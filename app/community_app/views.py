5ab73687eb33c6ae327b3fbded9184663ca29228
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-05-17 20:46:38 +0200

    User can now like posts

--- app/community_app/views.py
+++ app/community_app/views.py
@@ -30,18 +30,16 @@ def community(request):
                     post_id = post
                 )
         elif 'like' in request.POST:
-            print("like")
-            print("METHOD: POST")
-            likeForm = LikeForm(request.POST)
-            print(request.POST)
-            print(likeForm)                
-            if form.is_valid():
-                d = form.cleaned_data
-                print("IN")
-                PostLikes.objects.create(
-                    user_id = CustomUser.objects.get(id=d.get("user_id")),
-                    post_id = Posts.objects.get(id=d.get("post_id"))
-                    )
+            likeForm = LikeForm(request.POST)              
+            if likeForm.is_valid():
+                d = likeForm.cleaned_data
+                if PostLikes.objects.filter(user_id=d.get("user_id"), post_id=d.get("post_id")).exists():
+                    PostLikes.objects.filter(user_id=d.get("user_id"), post_id=d.get("post_id")).delete()
+                else:
+                    PostLikes.objects.create(
+                        user_id = CustomUser.objects.get(id=d.get("user_id")),
+                        post_id = Posts.objects.get(id=d.get("post_id"))
+                        )
 
 
     posts = Posts.objects.all()
@@ -54,9 +52,12 @@ def convertPosts(posts):
     for post in posts:
         user = CustomUser.objects.get(id=post.user_id.id).username
         asset = Asset.objects.get(name=post.asset.name).name
+        post = Posts.objects.get(id=post.id)
+        post_id = Posts.objects.get(id=post.id).id
         content = post.content
         created_at = post.created_at
         hashtags = post.hashtags
-        postObject = {"user":user, "asset":asset, "content":content, "created_at":created_at, "hashtags":hashtags}
+        likes = PostLikes.objects.filter(post_id = post).count()
+        postObject = {"user":user, "asset":asset, "content":content, "created_at":created_at, "hashtags":hashtags, "likes": likes, "post_id": post_id}
         convertedPosts.append(postObject)
     return convertedPosts
