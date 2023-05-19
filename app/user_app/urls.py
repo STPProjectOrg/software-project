6d90d9b918364660c9abd85fc2d9fce4a37026f5
Maximilian Steinberg <maximilian.steinberg@stud.hs-flensburg.de>
2023-05-19 14:33:05 +0200

    STP-140: Refactor entire Password reset

--- app/user_app/urls.py
+++ app/user_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from . import views
-from .forms import UserLoginForm
+from .forms import UserLoginForm, PasswordCustomResetForm, PasswordCustomSetForm
 
 app_name = 'user_app'
 
@@ -11,5 +11,25 @@ urlpatterns = [
     path('logout', auth_views.LogoutView.as_view(template_name = 'core/landing.html'), name='logout'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('update/userprofile/<int:pk>', views.UserProfileUpdateView.as_view(), name='update_userprofile'),
-    path('follow/<str:username>', views.toggle_follow, name='follow')  
+    path('follow/<str:username>', views.toggle_follow, name='follow'),
+    
+    # Password reset
+    path('password-reset/',
+        views.ResetPasswordView.as_view(
+        template_name="user_app/password_recovery/reset_password.html", 
+        form_class = PasswordCustomResetForm), 
+        name='reset_password'),
+    path('password-reset-sent/',
+        auth_views.PasswordResetDoneView.as_view(
+        template_name="user_app/password_recovery/reset_password_sent.html"), 
+        name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', 
+        views.ConfirmResetPasswordView.as_view(
+        template_name="user_app/password_recovery/reset_password_confirm.html", 
+        form_class = PasswordCustomSetForm), 
+        name='password_reset_confirm'),
+    path('password-reset-complete/', 
+        auth_views.PasswordResetCompleteView.as_view(
+        template_name="user_app/password_recovery/reset_password_complete.html"), 
+        name='password_reset_complete')
 ]
