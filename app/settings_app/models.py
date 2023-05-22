f0678b266fb502918b9f778fc723a5fb40d422e9
Nico Isheim <nico.isheim@tutanota.com>
2023-05-22 23:46:35 +0200

    Add userSettings functionality

--- app/settings_app/models.py
+++ app/settings_app/models.py
@@ -8,7 +8,6 @@ from user_app.models import CustomUser
 
 class Settings(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    settings = models.CharField(max_length=30, blank=False)
 
     def __str__(self):
         return self.settings
