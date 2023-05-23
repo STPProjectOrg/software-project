21f45af2b7c86e276b6e7997bbf7388c98a5726f
Maximilian Steinberg <maximilian.steinberg@stud.hs-flensburg.de>
2023-05-23 02:25:44 +0200

    Add profile follower listen und profile rework

--- app/user_app/views.py
+++ app/user_app/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse, reverse_lazy
 from user_app.models import CustomUser, UserProfileInfo, UserFollowing
@@ -9,16 +10,20 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
 from django.contrib.messages.views import SuccessMessageMixin
 
+
 class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
     template_name = 'users/password_reset.html'
     email_template_name = "user_app/password_recovery/reset_password_email.html",
     subject_template_name = "user_app/password_recovery/reset_password_email_subject",
     success_url = reverse_lazy('user_app:password_reset_done')
-    
+
+
 class ConfirmResetPasswordView(PasswordResetConfirmView):
     success_url = reverse_lazy('user_app:password_reset_complete')
-    
+
 # Create your views here.
+
+
 def register(request):
     registred = False
 
@@ -29,18 +34,18 @@ def register(request):
 
         if user_form.is_valid() and userprofile_form.is_valid():
 
-            # Save the user registration data from POST-form to the database and hash the password 
+            # Save the user registration data from POST-form to the database and hash the password
             new_user = user_form.save()
             new_user.set_password(new_user.password)
             new_user.save()
 
-            # Save the user-info registration data from POST-form to the database and connect them 
+            # Save the user-info registration data from POST-form to the database and connect them
             # to the previous user table
             new_profile = userprofile_form.save(commit=False)
             new_profile.user = new_user
 
             if 'profile_pic' in request.FILES:
-                new_profile.profile_pic = request.FILES['profile_pic'] 
+                new_profile.profile_pic = request.FILES['profile_pic']
             new_profile.save()
 
             registred = True
@@ -48,15 +53,15 @@ def register(request):
 
         else:
             print(user_form.errors, userprofile_form.errors)
-    
+
     else:
         user_form = UserRegistrationForm()
         userprofile_form = UserProfileInfoForm()
 
-    return render(request, 'user_app/registration.html', 
-                  {'user_form':user_form, 
-                   'profile_form':userprofile_form,
-                   'registred':registred})
+    return render(request, 'user_app/registration.html',
+                  {'user_form': user_form,
+                   'profile_form': userprofile_form,
+                   'registred': registred})
 
 
 @login_required
@@ -65,55 +70,63 @@ def profile_redirect(request):
     profile_url = reverse('user_app:profile', kwargs={'username': username})
     return redirect(profile_url)
 
+
 @login_required
 def profile(request, username):
 
     # Check if the current profile is it's own profile or not an get the user from DB
-    is_own_profile = False
-    if username == request.user.username:
-        profile_user = CustomUser.objects.get(username=request.user.username)
-        is_own_profile = True
-    else:
-        profile_user = get_object_or_404(CustomUser, username=username)
+    profile_user = get_object_or_404(
+        CustomUser, username=username)
+    is_own_profile = request.user.username == profile_user.username
 
-    # Get followers and following
-    get_follower_count = profile_user.followers.count()
-    get_following_count = profile_user.following.count()
-    is_following = request.user.following.all().filter(following_user=profile_user).exists()
+    # following_list = CustomUser.objects.filter(id=1)
+    followers = CustomUser.objects.filter(
+        following__following_user_id=profile_user.id).select_related("userprofileinfo")
+    following = CustomUser.objects.filter(
+        followers__follower_user_id=profile_user.id).select_related("userprofileinfo")
 
-    # Get the picture url. When user doesn't have one get the default picture
+    own_following = request.user.following.values_list(
+        "following_user_id", flat=True)
+
+    # is_followig und picture_url direct im Objekt speichern
+    is_following = request.user.following.all().filter(
+        following_user=profile_user).exists()
     picture_url = profile_user.userprofileinfo.profile_pic.url if profile_user.userprofileinfo.profile_pic else "http://ssl.gstatic.com/accounts/ui/avatar_2x.png"
 
-    return render(request,'user_app/profile2.html', 
-                  {"user_id": profile_user.id,
+    # Get the picture url. When user doesn't have one get the default picture
+
+    return render(request, 'user_app/profile.html',
+                  {"profile_user": profile_user,
                    "user_profile_id": profile_user.userprofileinfo.id,
-                   "user_name": profile_user.username,
-                   "first_name": profile_user.first_name,
-                   "last_name": profile_user.last_name,
-                   "email": profile_user.email,
                    "picture_url": picture_url,
                    "is_own_profile": is_own_profile,
-                   "followers": get_follower_count,
-                   "following": get_following_count,
                    "is_following": is_following,
+                   "followers": followers,
+                   "following": following,
+                   "own_following": own_following,
                    })
+
 
 @login_required
 def toggle_follow(request, username):
     profile_user = CustomUser.objects.get(username=request.user.username)
     other_user = get_object_or_404(CustomUser, username=username)
-    
+
     if profile_user != other_user:
         if profile_user.following.all().filter(following_user=other_user).exists():
-            UserFollowing.objects.filter(follower_user=profile_user, following_user=other_user).delete()
+            UserFollowing.objects.filter(
+                follower_user=profile_user, following_user=other_user).delete()
         else:
-            UserFollowing.objects.create(follower_user=profile_user, following_user=other_user)
+            UserFollowing.objects.create(
+                follower_user=profile_user, following_user=other_user)
 
-    return redirect(reverse('user_app:profile', kwargs={"username": username}))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 def getUser(id):
     user = CustomUser.objects.get(id=id)
     return user
+
 
 class UserProfileUpdateView(LoginRequiredMixin, UpdateView):
     model = UserProfileInfo
@@ -122,19 +135,22 @@ class UserProfileUpdateView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-    
+
     def get_success_url(self):
         return reverse('user_app:profile', kwargs={"username": "self"})
 
-@login_required 
+
+@login_required
 def follower_list(request, username):
     profile_user = CustomUser.objects.get(username=username)
     follower_list = []
     for follower in UserFollowing.objects.filter(following_user_id=profile_user.id):
         follower_user = CustomUser.objects.get(id=follower.follower_user_id)
         user_picture = profile_user.userprofileinfo.profile_pic.url if profile_user.userprofileinfo.profile_pic else "http://ssl.gstatic.com/accounts/ui/avatar_2x.png"
-        is_following = UserFollowing.objects.filter(following_user_id=follower.follower_user.id).filter(follower_user_id=request.user.id).exists()
-        followerData = {"username": follower_user.username, "user_picture": user_picture, "is_following": is_following, "followers": follower_user.followers.count()}
+        is_following = UserFollowing.objects.filter(
+            following_user_id=follower.follower_user.id).filter(follower_user_id=request.user.id).exists()
+        followerData = {"username": follower_user.username, "user_picture": user_picture,
+                        "is_following": is_following, "followers": follower_user.followers.count()}
         follower_list.append(followerData)
     data = {"follower": follower_list}
-    return  render(request,'user_app/follower_list.html', context=data)
+    return render(request, 'user_app/follower_list.html', context=data)
