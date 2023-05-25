73a78746ddc3ef153e8092be415af71c589eb221
Maximilian Steinberg <maximilian.steinberg@stud.hs-flensburg.de>
2023-05-25 20:55:07 +0200

    Add navigation routes

--- app/core/templatetags/core_tags.py
+++ app/core/templatetags/core_tags.py
@@ -1,18 +1,22 @@
 """ Custom Tags for the "Core Application" """
 
 from django import template
+from django.urls import reverse
 
 register = template.Library()
 
 # inclusion_tags
 
+
 @register.inclusion_tag("inclusion/search_modal.html")
 def search_modal():
-    return 
+    return
+
 
 @register.inclusion_tag("inclusion/search_result.html")
 def search_result(username):
     return {'username': username}
+
 
 @register.inclusion_tag("inclusion/logo_name.html")
 def logo_name():
@@ -35,9 +39,21 @@ def navigation():
 
 
 @register.inclusion_tag("inclusion/navigation_button.html")
-def navigation_button(name, route, icon):
+def navigation_button(name, icon, route, *args, **kwargs):
+    """
+    Include a navigation button.
+
+    Keyword arguments:
+        name: The name to be displayed.
+        icon: The icon to be displayed.
+        route: The url of the given page.
+        args, kwargs: Additional route parameter.
+    """
+
+    resolved_route = reverse(route, None, args, kwargs)
+
     return {'name': name,
-            'route': route,
+            'route': resolved_route,
             'icon': icon}
 
 
