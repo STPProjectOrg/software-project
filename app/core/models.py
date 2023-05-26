e61067641f55281d696a23b24d5792469c814d72
Maximilian Steinberg <maximilian.steinberg@stud.hs-flensburg.de>
2023-05-26 16:33:39 +0200

    Add core_app refactor

--- app/core/models.py
+++ app/core/models.py
@@ -1,17 +1,20 @@
+""" Models for the core_app """
 
 # Create your models here.
 from django.db import models
+from app.user_app.models import CustomUser
 
-from user_app.models import CustomUser
 
 class Asset(models.Model):
-    assetname = models.CharField(max_length= 50)
+    assetname = models.CharField(max_length=50)
+
 
 class Receipt(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
     amount = models.FloatField()
     buydate = models.DateTimeField("date bought")
+
 
 class History(models.Model):
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
