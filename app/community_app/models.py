a53b554a329678dc341c7e40020e4e69291a3f8f
Maximilian Steinberg <maximilian.steinberg@stud.hs-flensburg.de>
2023-05-27 17:34:51 +0200

    Post Date -> DateTime

--- app/community_app/models.py
+++ app/community_app/models.py
@@ -7,7 +7,7 @@ from api_app.models import Asset
 class Post(models.Model):
     user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
-    created_at = models.DateField()
+    created_at = models.DateTimeField()
     tags = models.CharField(max_length=100)
     # privacySettings = models.CharField()
 
