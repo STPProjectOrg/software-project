2b530aa5a3e6b5652d68bd72bcfbfffd506ca1ce
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-05-29 16:33:55 +0200

    unread messages count gets now displayed

--- app/messaging_app/models.py
+++ app/messaging_app/models.py
@@ -10,7 +10,7 @@ class Inbox(models.Model):
 class InboxParticipants(models.Model):
     inbox_id = models.ForeignKey(Inbox, on_delete=models.CASCADE)
     participant_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    last_message = models.CharField(max_length=100)
+    last_message = models.CharField(max_length=30)
     last_message_sent_at = models.DateTimeField(null=True, blank=True)
     def __str__(self):
         return self.inbox_id
@@ -19,4 +19,5 @@ class Message(models.Model):
     from_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="from_user")
     to_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="to_user")
     message = models.CharField(max_length=1000)
+    message_read = models.BooleanField()
     created_at = models.DateTimeField()
