8ae768189c4462274651505cfba37f5edf840c63
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-06-05 18:22:39 +0200

    you can now tag people or coins in a message

--- app/messaging_app/templatetags/messaging_tags.py
+++ app/messaging_app/templatetags/messaging_tags.py
@@ -1,8 +1,10 @@
-""" Custom Tags for the core_app """
+""" Custom Tags for the messaging_app """
 
-from datetime import datetime
 from django import template
-
+from django.template.defaultfilters import stringfilter
+from django.utils.html import mark_safe
+from user_app.models import CustomUser
+from api_app.models import Asset
 
 register = template.Library()
 
@@ -12,3 +14,16 @@ def full_image_modal(message):
 
     return {"message":message}
 
+@register.filter
+@stringfilter
+def tag(value : str):
+    if "@" in value:
+        cut = value.split(" ")
+        for current in cut:
+            if current[0] == "@":
+                if CustomUser.objects.filter(username=current[1:current.__len__()]).exists():
+                    value = value.replace(current, f'<a href="http://localhost:8000/auth/profile/{current[1:current.__len__()]}/">{current}</a>')
+                elif Asset.objects.filter(name=current[1:current.__len__()].upper()).exists():
+                    value = value.replace(current, f'<a href="http://localhost:8000/dashboard_app/asset/{current[1:current.__len__()].lower()}">{current}</a>')
+
+    return mark_safe(value)
