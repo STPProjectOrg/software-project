c4fa1fb2ee300c09efca16eabd0be298aaa2753b
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-06-20 07:47:31 +0200

    tags are now useable in posts and comments

--- app/community_app/templatetags/community_tags.py
+++ app/community_app/templatetags/community_tags.py
@@ -2,7 +2,10 @@
 
 from datetime import datetime
 from django import template
-
+from django.template.defaultfilters import stringfilter
+from django.utils.html import mark_safe
+from user_app.models import CustomUser
+from api_app.models import Asset
 
 register = template.Library()
 
@@ -48,3 +51,17 @@ def full_image_modal(post):
     """ Include a modal for post creation. """
 
     return {"post": post}
+
+@register.filter
+@stringfilter
+def tag(value : str):
+    if "@" in value:
+        cut = value.split(" ")
+        for current in cut:
+            if current[0] == "@":
+                if CustomUser.objects.filter(username=current[1:current.__len__()]).exists():
+                    value = value.replace(current, f'<a href="http://localhost:8000/auth/profile/{current[1:current.__len__()]}/">{current}</a>')
+                elif Asset.objects.filter(name=current[1:current.__len__()].upper()).exists():
+                    value = value.replace(current, f'<a href="http://localhost:8000/dashboard_app/asset/{current[1:current.__len__()].lower()}">{current}</a>')
+
+    return mark_safe(value)
