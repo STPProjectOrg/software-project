b09fc6831c1d8c48e0f1c8a3f8de1daefebdf5d5
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-06-20 11:41:51 +0200

    translated community app

--- app/community_app/views/main.py
+++ app/community_app/views/main.py
@@ -4,8 +4,8 @@
 from django.shortcuts import render
 from community_app.forms import PostForm
 from community_app.views import post
-
-
+from django.http import HttpResponseRedirect
+from django.utils import translation
 # Create your views here.
 
 def community(request, feed):
@@ -18,10 +18,14 @@ def community(request, feed):
 
     form = PostForm()
     posts = post.get_by_feed(feed, user_id=request.user.id)
-    
+
     data = {
         'form': form,
         'posts': posts
     }
 
     return render(request, 'community_app/community.html', context=data)
+
+def language_change(request, language_code):
+    translation.activate(language_code)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
