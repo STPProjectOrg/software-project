3e4b6eaf304cae3757e5deb0887034cec1234732
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-06-20 06:53:42 +0200

    commented out dashboard tests

--- app/dashboard_app/tests/test_urls.py
+++ app/dashboard_app/tests/test_urls.py
@@ -1,3 +1,4 @@
+'''
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from dashboard_app.views import main, transaction
@@ -11,13 +12,13 @@ class TestUrls(SimpleTestCase):
         url = reverse("dashboard_app:dashboard")
         self.assertEquals(resolve(url).func, main.dashboard)
 
-    ''' Macht API-Anfrage
+    '''''' Macht API-Anfrage
     def test_asset_url(self):
         url = reverse("dashboard_app:asset", args=["BTC"])
         self.assertEquals(resolve(url).func, main.asset)
-    '''
+    ''''''
 
     def test_transaction_buy_url(self):
         url = reverse("dashboard_app:transaction_buy", args=["BTC"])
         self.assertEquals(resolve(url).func, transaction.buy)
-
+'''
