1d35adfc65b448a6de3cd8e55fd5e46a6b26cce2
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-06-24 11:23:51 +0200

    variable price_change

--- app/dashboard_app/views/watchlist.py
+++ app/dashboard_app/views/watchlist.py
@@ -25,7 +25,10 @@ def get_watchlist(user):
         date="2023-05-20"
         d=datetime.strptime(date, "%Y-%m-%d").date()
         price = AssetHistory.objects.get(name_id=asset.id, date=date)
-        pricediff = AssetHistory.objects.get(name_id=asset.id, date=d-timedelta(days=1))
+        try:
+            pricediff = AssetHistory.objects.get(name_id=asset.id, date=d-timedelta(days=watchlist_asset.price_change))
+        except:
+            pricediff = AssetHistory.objects.get(name_id=asset.id, date=d-timedelta(days=0))
         pricediffpercent = round((price.value / pricediff.value) - 1,4)
         data = {
             "imageUrl": asset.imageUrl,
@@ -35,10 +38,15 @@ def get_watchlist(user):
             "pricediff": pricediff, 
             "pricediffpercent":pricediffpercent,
             "added_at": watchlist_asset.added_at,
-            "isInPortfolio": Transaction.objects.filter(user=user, asset=asset).exists()
+            "isInPortfolio": Transaction.objects.filter(user=user, asset=asset).exists(),
+            "price_change": watchlist_asset.price_change
             }
         assets.insert(0, data)
     return assets
 
 
-    
+def watchlist_update_asset_price_change(request, asset_symbol, price_change):
+    asset = Asset.objects.get(name= asset_symbol)
+    Watchlist.objects.filter(user=request.user, asset=asset).update(price_change=price_change)
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
