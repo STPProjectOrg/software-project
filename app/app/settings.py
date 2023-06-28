40600c053404b91fa51e4e558cd3a5c32609ffa7
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-06-28 14:23:48 +0200

    changed settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': Path(__file__).resolve().parent.parent.parent / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
