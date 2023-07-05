31256c26d6d307160e514b4b5affece07f13575b
Nico Isheim <nico.isheim@tutanota.com>
2023-07-05 21:00:26 +0200

    Reformat models.py

--- app/notification_app/models.py
+++ app/notification_app/models.py
@@ -24,7 +24,7 @@ class Notification(models.Model):
     notification_type = models.CharField(
         max_length=20, choices=NOTIFICATION_TYPES)
     message = models.TextField()
-    read = models.BooleanField(default=False)
+    status = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
