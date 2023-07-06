c60552ebc5c662fdb59fe31836178219da4e99b6
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-07-06 13:39:08 +0200

    add redis port

--- app/app/settings.py
+++ app/app/settings.py
@@ -100,7 +100,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("127.0.0.1", 6379), ("139.144.176.245:8000", 6379)],
         },
     },
 }
