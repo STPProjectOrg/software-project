4ea76be52a40f36e23f8af5f2afb338ec47ee7c1
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-07-09 14:27:37 +0200

    db

--- app/app/settings.py
+++ app/app/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
 
     'default': {
 
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
         'NAME': 'postgres',
 
