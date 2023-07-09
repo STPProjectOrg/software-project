a26ca11afa8589950e0413aa0d5ce6e2acefb94c
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-07-09 14:57:34 +0200

    add psycopg2

--- app/app/settings.py
+++ app/app/settings.py
@@ -102,7 +102,7 @@ if TESTING==False:
 
         'default': {
 
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
             'NAME': 'postgres',
 
