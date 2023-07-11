652a3b19c8636adba024ef1fc11a47b5d01b7a03
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-07-11 11:55:04 +0200

    fix for api form

--- app/api_app/forms.py
+++ app/api_app/forms.py
@@ -2,12 +2,11 @@ from django import forms
 import datetime
 from .databaseservice import get_asset_choices
 
-asset_choices = get_asset_choices()
 currency_choices = [("USD","USD"), ("EUR","EUR"), ("GBP","GBP"), ("JPY","JPY"), ("KRW","KRW")]
 
 
 class AddAssetData(forms.Form):
-    asset = forms.CharField(label="Cryptocoin Symbol",widget=forms.Select(choices=asset_choices, attrs={"class": "form-control"}), initial="EUR", required=True)
+    asset = forms.CharField(label="Cryptocoin Symbol",widget=forms.Select(choices=get_asset_choices(), attrs={"class": "form-control"}), initial="EUR", required=True)
     currency = forms.CharField(label="Währung",widget=forms.Select(choices=currency_choices, attrs={"class": "form-control"}), initial="EUR", required=True)
     dateFrom = forms.DateField(label="Von:", widget = forms.widgets.DateInput(attrs={'type': 'date', "class": "form-control",'value': datetime.date.today()}), required=True)
     dateTo = forms.DateField(label="Bis:", widget = forms.widgets.DateInput(attrs={'type': 'date', "class": "form-control",'value': datetime.date.today()}),required=True)
