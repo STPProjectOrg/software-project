8a075440c6223dbc7ab897c1128896679098cc7f
dido3296 <dimitri.dorn2@stud.hs-flensburg.de>
2023-08-07 20:03:30 +0200

    Added comments in forms.py

--- app/user_app/forms.py
+++ app/user_app/forms.py
@@ -9,6 +9,30 @@ invalid_username_msg = "Benutzername ist bereits vergeben!"
 
 
 class UserRegistrationForm(forms.ModelForm):
+    """
+    ModelForm for user registration.
+
+    This form is used to handle user registration data input. It includes fields for username,
+    password, password confirmation, first name, last name, and email. The form provides custom
+    widgets and placeholder text for each field.
+
+    Attributes:
+        username (CharField): Field for the username input.
+        password (CharField): Field for the password input.
+        password2 (CharField): Field for the password confirmation input.
+        first_name (CharField): Field for the user's first name input.
+        last_name (CharField): Field for the user's last name input.
+        email (CharField): Field for the user's email input.
+
+    Methods:
+        set_form_control(): Sets the appropriate CSS class for each form field based on its validity.
+        clean(self): Custom form validation for password matching.
+
+    Meta:
+        model (CustomUser): The model associated with the form.
+        fields (tuple): The fields to be included in the form.
+    """
+    # Form fields with custom widget attributes
     username = forms.CharField(widget=forms.TextInput(attrs={
         'id': 'username',
         'class': 'form-control',
@@ -73,6 +97,16 @@ class UserRegistrationForm(forms.ModelForm):
 
 
 class UserLoginForm(AuthenticationForm):
+    """
+    Custom login form for user authentication. This class represents 
+    a custom login form that inherits from Django's built-in AuthenticationForm.
+    It provides additional customization to the form fields, 
+    such as widget attributes.
+
+    Attributes:
+        username (CharField): A field to capture the user's username.
+        password (CharField): A field to capture the user's password.
+    """
     username = forms.CharField(widget=forms.TextInput(attrs={
         'id': 'username',
         'class': 'form-control',
@@ -90,6 +124,16 @@ class UserLoginForm(AuthenticationForm):
 
 
 class PasswordCustomResetForm(PasswordResetForm):
+     """
+    Custom password reset form for resetting user passwords.
+    This class represents a custom password reset form that inherits 
+    from Django's built-in PasswordResetForm.
+
+    Attributes:
+        email (EmailField): A field to capture the user's email address for password reset. 
+            The field is also marked as required, ensuring that the user must provide an 
+            email address for the password reset request.
+    """
     email = forms.EmailField(widget=forms.EmailInput(attrs={
         'id': 'id_email',
         'class': 'form-control',
@@ -98,6 +142,20 @@ class PasswordCustomResetForm(PasswordResetForm):
 
 
 class PasswordCustomSetForm(SetPasswordForm):
+     """
+    Custom password set form for setting a new user password.
+    This class represents a custom password set form that inherits 
+    from Django's built-in SetPasswordForm.
+
+    Attributes:
+        new_password1 (CharField): A field to capture the user's new password. 
+            The field is also marked as required, ensuring that the user must 
+            provide a new password.
+
+        new_password2 (CharField): A field to confirm the user's new password. 
+            The field is also marked as required, ensuring that the user must 
+            confirm the new password by entering it again.
+    """
     new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={
         'id': 'id_new_password1',
         'class': 'form-control',
