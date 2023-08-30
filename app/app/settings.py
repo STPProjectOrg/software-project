650687e7d4c1f1590b65164c741b9ec34ec98d37
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-08-30 14:52:34 +0200

    Add IP

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-hzdef6(rps5z3=$s0o_qizd74g*osd&rl^dbai!c*74rx4q_e6
 DEBUG = True
 
 ALLOWED_HOSTS = ['https://websocketking.com',
-                 '143.42.31.113', '0.0.0.0', 'localhost', '127.0.0.1']
+                 '139.144.178.254', '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
