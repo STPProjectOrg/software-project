1f7f44b1be1aa77aeb6fce9c4e6cd93b87a841f6
Nico Isheim <nico.isheim@tutanota.com>
2023-09-18 21:26:09 +0200

    Add message received notification

--- app/messaging_app/views/messages.py
+++ app/messaging_app/views/messages.py
@@ -1,9 +1,15 @@
 from messaging_app.models import Message
 from django.http import HttpResponseRedirect
 from messaging_app.forms import AddMessageForm
+from notification_app.methods import send_notification
 from user_app.models import CustomUser
 from datetime import datetime
 from .participants import create_or_update_inbox_participant
+from channels.layers import get_channel_layer
+
+
+channel_layer = get_channel_layer()
+
 
 def create_message(request, chat_participant_name):
     """
@@ -18,18 +24,24 @@ def create_message(request, chat_participant_name):
     if form.is_valid():
         data = form.cleaned_data
         Message.objects.create(
-            from_user = request.user,
-            to_user = chat_participant,
-            message = data["message"],
-            message_read = False,
-            created_at = datetime.now(),
-            image = data["image"]
+            from_user=request.user,
+            to_user=chat_participant,
+            message=data["message"],
+            message_read=False,
+            created_at=datetime.now(),
+            image=data["image"]
         )
 
-    create_or_update_inbox_participant(request.user, chat_participant, data["message"])
-    create_or_update_inbox_participant(chat_participant, request.user, data["message"])
+    create_or_update_inbox_participant(
+        request.user, chat_participant, data["message"])
+    create_or_update_inbox_participant(
+        chat_participant, request.user, data["message"])
+
+    send_notification(channel_layer, chat_participant.pk, "message",
+                      f"{request.user.username} send you a message.")
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
 
 def get_chat_messages(user, chat_participant):
     """
@@ -39,19 +51,20 @@ def get_chat_messages(user, chat_participant):
         user:               The user that is signed in.
         chat_participant:   The chat participant.
     """
-        
+
     chat_messages_user_logged_in = Message.objects.filter(
         from_user=user, to_user=chat_participant)
-    
+
     chat_messages_participant = Message.objects.filter(
         from_user=chat_participant, to_user=user)
-    
+
     chat_messages_combined = (chat_messages_user_logged_in |
-                     chat_messages_participant).order_by('created_at')
-    
+                              chat_messages_participant).order_by('created_at')
+
     update_message_read(chat_messages_combined, chat_participant)
 
     return reversed(chat_messages_combined)
+
 
 def update_message_read(messages, chat_participant):
     """
@@ -61,7 +74,8 @@ def update_message_read(messages, chat_participant):
         messages:           All messages between the users.
         chat_participant:   The chat participant.
     """
-        
+
     for mes in messages:
         if mes.message_read == False:
-            Message.objects.filter(id=mes.id, from_user=chat_participant).update(message_read = True)
+            Message.objects.filter(
+                id=mes.id, from_user=chat_participant).update(message_read=True)
