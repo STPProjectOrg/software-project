73a771705a4c7b1c5ccfc239bc26c68d1fe3afb0
Julian Mueller <julian.mueller@stud.hs-flensburg.de>
2023-09-18 13:42:07 +0200

    Update Dashboard Tests

--- app/dashboard_app/tests/test_urls.py
+++ app/dashboard_app/tests/test_urls.py
@@ -1,25 +1,18 @@
-'''
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from dashboard_app.views import main, transaction
 
-
-
-
 class TestUrls(SimpleTestCase):
 
     def test_dashboard_url(self):
-        url = reverse("dashboard_app:dashboard")
+        url = reverse("dashboard_app:dashboard", args=[0])
         self.assertEquals(resolve(url).func, main.dashboard)
 
-    '''''' Macht API-Anfrage
     def test_asset_url(self):
-        url = reverse("dashboard_app:asset", args=["BTC"])
+        url = reverse("dashboard_app:asset", args=["BTC",0])
         self.assertEquals(resolve(url).func, main.asset)
-    ''''''
 
     def test_transaction_buy_url(self):
-        url = reverse("dashboard_app:transaction_buy", args=["BTC"])
-        self.assertEquals(resolve(url).func, transaction.buy)
+        url = reverse("dashboard_app:transaction_add", args=["BTC"])
+        self.assertEquals(resolve(url).func, transaction.add)
 
-'''
--- app/dashboard_app/tests/test_views.py
+++ app/dashboard_app/tests/test_views.py
@@ -3,36 +3,37 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 class TestViews(TestCase):
- '''
+
     #Test User erstellen und einloggen
     def setUp(self):
         User = get_user_model()
         self.user = User.objects.create_user("test", "test@test.de", "test")
         self.client = Client()
 
+
     def test_dashboard_page_unauthorized_html(self):
-        response = self.client.get(reverse("dashboard_app:dashboard"))
+        response = self.client.get(reverse("dashboard_app:dashboard", kwargs={"timespan" : 0}))
 
         assert response.status_code == 302, 'Should redirect to login page'
         assert 'login' in response.url
    
     def test_dashboard_page_authorized_html(self):
         self.client.login(username='test', password='test')
-        response = self.client.get(reverse("dashboard_app:dashboard"))
+        response = self.client.get(reverse("dashboard_app:dashboard", kwargs={"timespan" : 0}))
 
         assert response.status_code == 200, 'Status code should be 200'
         self.assertTemplateUsed(response, 'dashboard_app/dashboard.html')
 
-     Stellt API-Anfrage
     def test_asset_page_unauthorized_html(self):
-        response = self.client.get(reverse("dashboard_app:asset", kwargs={"coin": "btc"}))
+        response = self.client.get(reverse("dashboard_app:asset", args=["BTC",0]))
 
         assert response.status_code == 302, 'Should redirect to login page'
         assert 'login' in response.url
 
+    '''Benötigt Asset in der Testdatenbank, vebraucht API-Anfrage
     def test_asset_page_authorized_html(self):
         self.client.login(username='test', password='test')
-        response = self.client.get(reverse("dashboard_app:asset", kwargs={"coin": "btc"}))
+        response = self.client.get(reverse("dashboard_app:asset", args=["BTC",0]))
 
         assert response.status_code == 200, 'Status code should be 200'
         self.assertTemplateUsed(response, 'dashboard_app/asset.html')
