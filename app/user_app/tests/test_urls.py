66e9a9fc1411f25481d85b2ea836167ccc82d5db
dido3296 <dimitri.dorn2@stud.hs-flensburg.de>
2023-09-20 19:28:46 +0200

    Fixed Password Reset and urls-tests in user_app

--- app/user_app/tests/test_urls.py
+++ app/user_app/tests/test_urls.py
@@ -1,6 +1,5 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from user_app.views import register, profile_redirect, profile, ProfilePicUpdateView, delete_profile_pic, toggle_follow, follower_list
 
 
 from django.test import TestCase
@@ -17,18 +16,15 @@ class UserAppUrlsTestCase(TestCase):
             email='test@example.com',
             password='testpassword'
         )
-        self.client.login(username='testuser', password='testpassword')
-        self.profile = UserProfileInfo.objects.create(user=self.user)
-        self.banner = ProfileBanner.objects.create(user=self.user)
         self.other_user = User.objects.create_user(
             username='otheruser',
             email='other@example.com',
             password='otherpassword'
         )
-
-    def test_profile_redirect(self):
-        response = self.client.get(reverse('user_app:profile_redirect'))
-        self.assertEqual(response.status_code, 200)
+        self.client.login(username='testuser', password='testpassword')
+        self.profile = UserProfileInfo.objects.create(user=self.user)
+        self.banner = ProfileBanner.objects.create(user=self.user)
+        
 
     def test_profile(self):
         response = self.client.get(reverse('user_app:profile', args=['testuser', 0]))
@@ -36,23 +32,11 @@ class UserAppUrlsTestCase(TestCase):
 
     def test_follow(self):
         response = self.client.get(reverse('user_app:follow', args=['otheruser']))
-        self.assertEqual(response.status_code, 302)  # Should redirect
-
-    def test_update_userprofile(self):
-        response = self.client.get(reverse('user_app:update_userprofile', args=[self.profile.id]))
-        self.assertEqual(response.status_code, 200)
-
-    def test_update_userbanner(self):
-        response = self.client.get(reverse('user_app:update_userbanner', args=[self.banner.id]))
-        self.assertEqual(response.status_code, 200)
-
-    def test_update_biography(self):
-        response = self.client.get(reverse('user_app:update_biography'))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302) 
 
     def test_delete_userprofile(self):
         response = self.client.get(reverse('user_app:delete_userprofile', args=[self.profile.id]))
-        self.assertEqual(response.status_code, 302)  # Should redirect
+        self.assertEqual(response.status_code, 302) 
 
     def test_register(self):
         response = self.client.get(reverse('user_app:register'))
@@ -64,7 +48,7 @@ class UserAppUrlsTestCase(TestCase):
 
     def test_logout(self):
         response = self.client.get(reverse('user_app:logout'))
-        self.assertEqual(response.status_code, 302)  # Should redirect
+        self.assertEqual(response.status_code, 302) 
 
     def test_reset_password(self):
         response = self.client.get(reverse('user_app:reset_password'))
@@ -83,44 +67,5 @@ class UserAppUrlsTestCase(TestCase):
     def test_password_reset_complete(self):
         response = self.client.get(reverse('user_app:password_reset_complete'))
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user_app/password_recovery/reset_password_complete.html')
 
-# class TestUrls(SimpleTestCase):
-
-#     def test_register_url(self):
-#         url = reverse("user_app:register")
-#         self.assertEquals(resolve(url).func, register)
-
-#     '''
-#     def test_login_url(self):
-#         url = reverse("user_app:register")
-#         self.assertEquals(resolve(url).func, register)
-#     def test_logout_url(self):
-#         url = reverse("user_app:register")
-#         self.assertEquals(resolve(url).func, register)
-#     '''
-    
-#     def test_profile_redirect_url(self):
-#         url = reverse("user_app:profile_redirect")
-#         self.assertEquals(resolve(url).func, profile_redirect)
-
-#     ''' TODO Args 
-#     def test_profile_url(self):
-#         url = reverse("user_app:profile", args=[""])
-#         self.assertEquals(resolve(url).func, profile)
-
-#     def test_update_userprofile_url(self):
-#         url = reverse("user_app:update_userprofile", args=[""])
-#         self.assertEquals(resolve(url).func, ProfilePicUpdateView.as_view())
-
-#     def test_delete_profile_pic_url(self):
-#         url = reverse("user_app:delete/userprofile", args=[""])
-#         self.assertEquals(resolve(url).func, delete_profile_pic)
-
-#     def test_follow_url(self):
-#         url = reverse("user_app:follow", args=[""])
-#         self.assertEquals(resolve(url).func, toggle_follow)
-
-#     def test_follower_list_url(self):
-#         url = reverse("user_app:follower_list", args=[""])
-#         self.assertEquals(resolve(url).func, follower_list)
-#     '''
